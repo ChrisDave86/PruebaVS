5e984413534676af5eb111936e7c080cb36271a4
FABIAN <fabianchoque28@gmail.com>
2017-10-11 23:37:41 -0300

    comentarios

--- ProyectoGIT/ProyectoGIT/Class1.cs
+++ ProyectoGIT/ProyectoGIT/Class1.cs
@@ -6,8 +6,177 @@ using System.Threading.Tasks;
 
 namespace ProyectoGIT
 {
-    class Class1
+    //clase de electronico de Cristian
+    class Electrodomestico
     {
-        public void MetodoNuevo() => Console.WriteLine("Este es el constructor");
+        public enum Colores //es un tipo de vector que almacena solo esos datos
+        {
+            BLANCO,
+            NEGRO,
+            AZUL,
+            GRIS
+        };
+
+        public enum ConsumoEnergetico
+        {
+            A, B, C, D, E, F
+        };
+
+
+        private float precioBase;
+
+
+
+        private Colores colorElectrodomestico;//atributo del tipo del enum Colores
+
+
+
+        private ConsumoEnergetico consumoEnergetico;//atributo del tipo enum ConsumoEnergetico
+
+        private float peso;
+        private float precioFinal;
+
+
+
+        public Electrodomestico()//constructor por defecto
+        {
+
+            precioBase = 0f;
+            colorElectrodomestico = Colores.BLANCO;
+            consumoEnergetico = ConsumoEnergetico.A;
+            peso = 5f;
+
+        }
+
+        public Electrodomestico(float _precioBase, Colores _colorElectrodomestico, ConsumoEnergetico _consumoEnergetico, float _peso)//constructor con los atributos
+        {
+            precioBase = _precioBase;
+            colorElectrodomestico = _colorElectrodomestico;
+            consumoEnergetico = _consumoEnergetico;
+            peso = _peso;
+        }
+
+        public virtual void Mostrar()//funcion mostrar que tiene que ser llamada de cada clase de electrodomestico por eso usa la palabra virtual
+        {
+            Console.WriteLine("Color: {0}", colorElectrodomestico);
+            Console.WriteLine("Consumo energetico: {0}", consumoEnergetico);
+            Console.WriteLine("Peso: {0}", peso);
+            Console.WriteLine("Precio Base: {0}", precioBase);
+
+
+        }
+
+
+        public float PrecioBase
+        {
+            get
+            {
+                return precioBase;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    precioBase = value;
+                }
+
+
+            }
+        }
+
+        public float Peso
+        {
+            get
+            {
+                return peso;
+            }
+            set
+            {
+                peso = value;
+            }
+        }
+
+        public ConsumoEnergetico ConsumoEnerg
+        {
+            get
+            {
+                return consumoEnergetico;
+            }
+            set
+            {
+
+                if ((value != ConsumoEnergetico.A) && (value != ConsumoEnergetico.B) && (value != ConsumoEnergetico.C) && (value != ConsumoEnergetico.D) && (value != ConsumoEnergetico.E) && (value != ConsumoEnergetico.F))
+                {
+                    value = ConsumoEnergetico.F;
+                }
+
+                consumoEnergetico = value;
+            }
+        }
+
+        public Colores ColorElectrodomestico
+        {
+            get
+            {
+                return colorElectrodomestico;
+            }
+            set
+            {
+
+                if ((value != Colores.BLANCO) && (value != Colores.AZUL) && (value != Colores.GRIS) && (value != Colores.NEGRO))
+                {
+                    value = Colores.BLANCO;
+                }
+
+                colorElectrodomestico = value;
+            }
+        }
+
+        public float PrecioFinal
+        {
+            get
+            {
+                precioFinal = CalculoPrecioFinal();
+                return precioFinal;
+            }
+        }//fin propiedad PrecioFinal
+
+        //Metodo que calcula el precio final
+        public virtual float CalculoPrecioFinal()
+        {
+            float pf, valorConsumo = 0, valorPeso = 0;
+
+
+            switch (consumoEnergetico)
+            {
+                case ConsumoEnergetico.A: valorConsumo = 100; break;
+                case ConsumoEnergetico.B: valorConsumo = 80; break;
+                case ConsumoEnergetico.C: valorConsumo = 60; break;
+                case ConsumoEnergetico.D: valorConsumo = 50; break;
+                case ConsumoEnergetico.E: valorConsumo = 30; break;
+                case ConsumoEnergetico.F: valorConsumo = 10; break;
+                default: Console.WriteLine("Error: El valor del consumo energetico no es correcto."); break;
+            }
+
+            if ((peso > 0) && (peso < 19))
+            {
+                valorPeso = 10;
+            }
+            else if ((peso > 20) && (peso < 49))
+            {
+                valorPeso = 50;
+            }
+            else if ((peso > 50) && (peso < 79))
+            {
+                valorPeso = 80;
+            }
+            else if (peso > 80)
+            {
+                valorPeso = 100;
+            }
+
+            pf = precioBase + valorPeso + valorConsumo;
+            return pf;
+        }
     }
 }
--- ProyectoGIT/ProyectoGIT/Cocina.cs
+++ ProyectoGIT/ProyectoGIT/Cocina.cs
@@ -13,7 +13,7 @@ namespace ProyectoGIT
         int profundidad;
         string alimentacion;
 
-        public Cocina()
+        public Cocina()//constructor por defecto
         {
             this.Alto = 85;
             this.Ancho = 65;
@@ -21,7 +21,9 @@ namespace ProyectoGIT
             this.Alimentacion = "Gas";
         }
         public Cocina(double precioBase, String color, char consumo, double peso, int alto, int ancho, int profundidad, string alimentacion)
-            : base(precioBase, color, consumo, peso)
+            : base(precioBase, color, consumo, peso)//se asigna estas variables para que vayan a la clase Electrodomestico con la palabra base y entre ()
+            /*constructor con los atributos.. este es el metodo que se lo llama desde el MAIN y ponemos los atributos de 
+             */
         {
             this.Alto = alto;
             this.Ancho = ancho;
@@ -29,14 +31,18 @@ namespace ProyectoGIT
             this.Alimentacion = alimentacion;
         }
 
-        public int Alto { get => alto; set => alto = value; }
-        public int Ancho { get => ancho; set => ancho = value; }
-        public int Profundidad { get => profundidad; set => profundidad = value; }
+        public int Alto { get => alto; set => alto = value; }//estos son metodos de get y set que devuelven el valor que se encuentra en en dicha cocina
+        public int Ancho { get => ancho; set => ancho = value; }//pero no seran de utilidad en este programa.. solo lo ponemos porque lo dice el problema
+        public int Profundidad { get => profundidad; set => profundidad = value; }//estan con mayuscula al principio solo para difenciarlos de los atributos que estan en minuscula 
         public string Alimentacion { get => alimentacion; set => alimentacion = value; }
 
         public override double PrecioFinal()
+            /*(3) aqui es llamada del metodo de mostrar
+             se crea una variable precioFCocina y se le asigna lo que estaba como precio final en la clase electrodomestico(4)
+             y se hace las opercacion que qhace qye se le agregue mas precio a la cocina
+             finalmente devuelve un precio que seria el precio final de la cocina(5)*/
         {
-            double precioFCocina = base.PrecioFinal();
+            double precioFCocina = base.PrecioFinal();//(4)llama al metodo de la clase electrodomestico 
             if ((this.ancho >= 60) & (this.ancho < 80))
             { precioFCocina = precioFCocina + Math.Round((precioFCocina * 100 / 15), 2); ; }
             //la funcion math redondea el numero decimal a 2 decimales  
@@ -49,13 +55,20 @@ namespace ProyectoGIT
                     precioFCocina = precioFCocina + Math.Round((precioFCocina * 100 / 30), 2);
                 }
             }
-            return precioFCocina;
+            return precioFCocina;//(5)
         }
         public override void mostrar()
+         /*este procedimiento muestra los atributos de la cocina. Tiene que tener la palabra override para que sepa que usa
+          * un procedimiento de la clase Base que es la de electrodomestico. Primero el nombre COCINA para indicar que es una cocina
+          * despues llama a al metodo de la clase electrodomestico tienen que tener el mismo nombre  y la palabra base.(2)
+          para que primero muestre el precioBase el consumo el peso
+          luego vuelve a este procedimiento y terminar de mostrar los atributos que le falta a la cocina que son el alto ancho profundidad alimentacion y el precio final llamando
+          a la funcion de arriba(3)*/   
         {
             Console.Write("COCINA ");
-            base.mostrar();
+            base.mostrar();//(2)
             Console.Write("alto: {0}, ancho: {1}, profundidad: {2}, alientacion: {3}, PRECIO FINAL: {4}$\n", this.alto, this.ancho, this.profundidad, this.alimentacion, PrecioFinal());
+                                                                                                         //ponemos this para indicar que son las atributos de la cocina
         }
     }
 }
